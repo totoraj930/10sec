087ed50664f0c9bca6ce9b9a87841054f32f146e
Reona Oshima <totoraj930@yahoo.co.jp>
2020-04-17 22:05:38 +0900

    状態をフラグで管理するように変更 & 変数定義でvarを使わないように変更

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var startTime,
+let startTime,
 	interval;
-var $count,
+let $count,
 	$button,
 	$rank,
 	$tweet;
-var cheat = false;
+let cheat = false;
+const STATUS_FLAG = {
+    start: 0,
+    stop: 1,
+    reset: 2
+};
+let statusFlag = STATUS_FLAG.start;
 
 $(function() {
 	$count = $("#count");
@@ -12,28 +18,30 @@ $(function() {
 	$rank = $("#rank");
 	$tweet = $("#tweet");
 
-	$button.on("click", onClickButton);
+    $button.on("click", onClickButton);
 });
 
 function onClickButton(event) {
-	var status = $(this).attr("data-status");
-	switch (status){
-		case "start":
+	switch (statusFlag){
+		case STATUS_FLAG.start:
 			startCount();
 			break;
-		case "stop":
+		case STATUS_FLAG.stop:
 			stopCount();
 			break;
-		default:
+		case STATUS_FLAG.reset:
 			resetCount();
-			break;
+            break;
+        default:
+            resetCount();
+            break;
 	}
 }
 
 function startCount() {
 	startTime = new Date().getTime();
 	interval = setInterval(function() {
-		var ms = new Date().getTime() - startTime;
+		const ms = new Date().getTime() - startTime;
 		updateCount(ms);
 		if (cheat && ms >= 10*1000) stopCount();
 		if (!cheat && !$count.hasClass("hide") && ms > 5*1000 && ms <= 6*1000) {
@@ -42,13 +50,13 @@ function startCount() {
 		} else if (!cheat && ms > 6*1000) {
 			clearInterval(interval);
 		}
-	}, 10);
-	$button.attr("data-status", "stop");
+    }, 10);
+    statusFlag = STATUS_FLAG.stop;
 	$button.text("ストップ");
 }
 
 function stopCount() {
-	var ms = new Date().getTime() - startTime;
+	const ms = new Date().getTime() - startTime;
 	updateCount(ms);
 	clearInterval(interval);
 
@@ -58,8 +66,8 @@ function stopCount() {
 
 	$rank.text(getRankMessage(ms));
 	$tweet[0].href = getTweetLink(ms);
-	$tweet.show();
-	$button.attr("data-status", "reset");
+    $tweet.show();
+    statusFlag = STATUS_FLAG.reset;
 	$button.text("リセット");
 }
 
@@ -72,8 +80,8 @@ function resetCount() {
 	$count.removeClass("hide");
 	$count.stop();
 	$count.animate({"opacity": 100}, 1000);
-	$tweet.hide();
-	$button.attr("data-status", "start");
+    $tweet.hide();
+    statusFlag = STATUS_FLAG.start;
 	$button.text("スタート");
 }
 
@@ -82,7 +90,7 @@ function updateCount(ms) {
 }
 
 function getRankMessage(ms) {
-	var msg = "";
+	let msg = "";
 	if (ms < 9000)
 		msg = "はやすぎ！";
 	else if (9000 <= ms && ms < 9500)
@@ -101,7 +109,7 @@ function getRankMessage(ms) {
 }
 
 function getTweetLink(ms) {
-	var tweetLink = "https://twitter.com/intent/tweet?url=${URL}&text=${TEXT}",
+	let tweetLink = "https://twitter.com/intent/tweet?url=${URL}&text=${TEXT}",
 		url = "http://totoraj930.github.io/10sec",
 		text = "10秒で止めるやつ！\n結果は、『${RESULT}秒』でした！\nみんなも挑戦しよう";
 	text = text.replace("${RESULT}", (ms/1000^0) + "." + ("0"+(ms%1000/10^0)).slice(-2));
